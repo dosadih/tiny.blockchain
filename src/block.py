3b2e265ef5fa2f504a6bf441b907f58f0eceb076
Damien Pretet <damien.pretet@me.com>
2017-08-02 17:32:55 +0200

    Add server source

--- src/block.py
+++ src/block.py
@@ -28,8 +28,11 @@ class Block:
 
 def create_genesis_block():
     """Manually construct a block with index zero and arbitrary previous hash"""
-    return Block(0, date.datetime.now(), "Genesis Block", "0")
-
+    return Block(0,
+                 date.datetime.now(),
+                 {"proof-of-work": 9,
+                  "transactions": None},
+                  "0")
 
 def next_block(last_block):
     """This function cosntructs a block of data"""
